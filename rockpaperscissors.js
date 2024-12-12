8519db394df545ae14f89b29fbd37218b7bf4eea
AndrewRetry <[andrew.retryans@gmail.com]>
2024-12-12 11:57:09 +0800

    Add playRound function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -10,7 +10,27 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let inputText = prompt("Rock/Paper/Scissors : ")
+    let inputText = prompt("Pick one! [Rock/Paper/Scissors] : ")
     const humanChoice = inputText.toLowerCase()
     return humanChoice
+}
+
+function playRound() {
+    const choiceArray = ["rock", "paper", "scissors"]
+    let humanChoice = getHumanChoice()
+    let computerChoice = getComputerChoice()
+    if (choiceArray.indexOf(humanChoice) != -1) {
+        alert("Invalid Choice, Please Try Again")
+    } else {
+        let humanChoiceIndex = choiceArray.indexOf(humanChoice)
+        let computerChoiceIndex = choiceArray.indexOf(computerChoice)
+
+        if (humanChoiceIndex == computerChoiceIndex) {
+            return `It's a tie!, both you and computer picked ${humanChoice}` 
+        } else if ((humanChoiceIndex + 1) % choiceArray.length == computerChoiceIndex) {
+           return `You lost! you picked ${humanChoice}, while computer picked ${computerChoice}` 
+        } else if ((computerChoiceIndex + 1) % choiceArray.length == humanChoiceIndex) {
+            return `You won! you picked ${humanChoice}, while computer picked ${computerChoice}`
+        }
+    }
 }
