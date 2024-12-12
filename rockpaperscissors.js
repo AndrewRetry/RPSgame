9185173a90e85a56bbce3c917d474ec2ef83e303
AndrewRetry <[andrew.retryans@gmail.com]>
2024-12-12 11:38:11 +0800

    Add getHumanChoice function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,11 +1,16 @@
 function getComputerChoice() {
-    randomChoice = Math.floor(Math.random() * 3)
+    let randomChoice = Math.floor(Math.random() * 3)
     if (randomChoice === 0) {
-        return "Rock"
+        return "rock"
     } else if (randomChoice === 1) {
-        return "Paper"
+        return "paper"
     } else {
-        return "Scissors"
+        return "scissors"
     }
 }
 
+function getHumanChoice() {
+    let inputText = prompt("Rock/Paper/Scissors : ")
+    const humanChoice = inputText.toLowerCase()
+    return humanChoice
+}
